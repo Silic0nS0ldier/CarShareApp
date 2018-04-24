dbe55bb27064db6d0abb7bbeb953b3f6adfe8d27
Jordan Mele <SiliconSoldier@outlook.com.au>
2018-04-24 17:37:37 +1000

    'logs' and 'images' table migrations

--- db/migrations/1.js
+++ db/migrations/1.js
@@ -1,5 +1,55 @@
+const Knex = require("knex");
+/**
+ * @param {Knex.Transaction} trx 
+ */
 async function Run(trx) {
-    return "migration comment"
+    // System logs table
+    await trx.schema.createTable("logs", table => {
+        table.comment("System logs.");
+        table.string("category")
+            .notNullable()
+            .comment("Entry category.");
+        table.integer("severity")
+            .notNullable()
+            .comment("Entry severity. 0 = Informational; 1 = Warning; 2 = Error;");
+        table.string("message")
+            .notNullable()
+            .comment("Entry message.");
+        table.timestamp("logged_at")
+            .defaultTo(trx.fn.now())
+            .comment("Entry timestamp.");
+    });
+
+    // Images table
+    await trx.schema.createTable("images", table => {
+        table.comment("Stores site images.");
+        table.increments("id")
+            .primary();
+        table.string("name")
+            .notNullable()
+            .comment("Image file name.");
+        table.string("extension")
+            .notNullable()
+            .comment("Image file extension.");
+        table.integer("size_bytes")
+            .notNullable();
+        table.timestamp("first_uploaded_at")
+            .defaultTo(trx.fn.now())
+            .notNullable()
+            .comment("When file was first uploaded to the database.");
+        table.binary("data")
+            .notNullable()
+            .comment("File data as binary.");
+        table.string("data_hash")
+            .notNullable()
+            .comment("Hash of file data, used to assist in duplicate detection.");
+    });
+
+    // Images dependency table
+    // If we had more time, this would be a table used to track where an image is being used, thus enabling clean up operations later for images that are unused.
+    // This would work in addition to the duplicate detection logic.
+
+    return "'logs' and 'images' tables created."
 }
 
 module.exports = Run;
