9c610df8cb9d947fc198391b7b929ffba58156d4
Jordan Mele <SiliconSoldier@outlook.com.au>
2018-05-07 22:36:59 +1000

    Unified configuration infrastructure

--- db/src/main.js
+++ db/src/main.js
@@ -1,5 +1,22 @@
 import { Model } from "objection";
 import Knex from "knex";
+import { readFileSync } from "fs";
+import objectMerge from "object-merge";
+
+// Load configuration
+const config = (() => {
+    const rawConfig = JSON.parse(readFileSync("../config/config.json", "utf8"));
+    switch (process.env.SCENARIO) {
+        case "production":
+            return rawConfig.production;
+        case "staging":
+            return objectMerge(rawConfig.production, rawConfig.staging);
+        case "development":
+            return objectMerge(rawConfig.production, rawConfig.staging, rawConfig.development);
+        default:
+            throw new Error("Environment variable SCENARIO must be set to production, staging or development.");
+    }
+})();
 
 // Initialise Knex, with a delay to account for MySQL quirks
 // Retry logic not possible due to unhandled async exceptions within libraries which cannot be caught up here a.k.a. "bad code"
--- img/src/main.js
+++ img/src/main.js
@@ -1,5 +1,22 @@
 import express from "express";
 import { ImageModel } from "../../db/src/index";
+import { readFileSync } from "fs";
+import objectMerge from "object-merge";
+
+// Load configuration
+const config = (() => {
+    const rawConfig = JSON.parse(readFileSync("../config/config.json", "utf8"));
+    switch (process.env.SCENARIO) {
+        case "production":
+            return rawConfig.production;
+        case "staging":
+            return objectMerge(rawConfig.production, rawConfig.staging);
+        case "development":
+            return objectMerge(rawConfig.production, rawConfig.staging, rawConfig.development);
+        default:
+            throw new Error("Environment variable SCENARIO must be set to production, staging or development.");
+    }
+})();
 
 // Initialise express application
 const app = express();
