2e07e614bcecb9b036aa3f2d2fa3e6bd7f207389
Jordan Mele <SiliconSoldier@outlook.com.au>
2018-05-14 22:53:43 +1000

    The one that got away...

--- api/src/index.js
+++ api/src/index.js
@@ -1,3 +1,56 @@
-// This allows us to use the new ESM syntax. https://medium.com/web-on-the-edge/tomorrows-es-modules-today-c53d29ac448c
-require = require("esm")(module)
-module.exports = require("./main.js")
+import objectMerge from "object-merge";
+import express from "express";
+import DB from "../../db";
+import { readFileSync } from "fs";
+import AuthRegister from "./routes/auth";
+
+// Load configuration
+const config = (() => {
+    const rawConfig = JSON.parse(readFileSync("../config/config.json", "utf8"));
+    switch (process.env.SCENARIO) {
+        case "production":
+            return rawConfig.production;
+        case "staging":
+            return objectMerge(rawConfig.production, rawConfig.staging);
+        case "development":
+            return objectMerge(rawConfig.production, rawConfig.staging, rawConfig.development);
+        default: {
+            if (process.env.DOCKER == true) throw new Error("Environment variable SCENARIO must be set to production, staging or development.");
+            else {
+                console.log("Assuming local non-docker development environment.");
+                return objectMerge(rawConfig.production, rawConfig.staging, rawConfig.development);
+            }
+        }
+    }
+})();
+
+async function Main() {
+    // Import required database bits
+    const FullDB = await DB(config);
+
+    // Initialise express application
+    const app = express();
+
+    // Register auth routes
+    await AuthRegister(FullDB);
+    
+    app.use("/:session_id", (req, res, next) => {
+        // 1. look for session in sessions table
+
+        // 2. if session exists ensure not expired
+            // Grab user from users table
+            // Add to res.locals
+        // 2. Else
+            // Return unauthorised response (400?)
+    });
+
+    if (process.env.DOCKER == true) {
+        app.listen(8088);
+    } else {
+        app.listen(8080);
+    }
+
+    console.log("API is ready.");
+}
+
+Main();
