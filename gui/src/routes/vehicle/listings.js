85f0065e865e75f4a9cc410f59a53b29b763f14c
Jordan Mele <SiliconSoldier@outlook.com.au>
2018-05-17 17:25:32 +1000

    Vehicles listings functional prototype

--- gui/src/routes/vehicle/listings.js
+++ gui/src/routes/vehicle/listings.js
@@ -1,12 +1,29 @@
-import {h, Component} from 'preact';
-import LayoutGrid from 'preact-material-components/LayoutGrid';
-import Icon from 'preact-material-components/Icon';
 import 'preact-material-components/LayoutGrid/style.css';
-
+import { h, Component } from 'preact';
+import Form from "../../components/Forms/Form";
+import Icon from 'preact-material-components/Icon';
+import LayoutGrid from 'preact-material-components/LayoutGrid';
 import style from './style';
+import TextField from "../../components/Forms/TextFormField";
 
 export default class VehicleListings extends Component {
-    render() {
+
+    populatePage = (response) => {
+        response.json()
+            .then(payload => {
+                this.setState({
+                    vehicles: payload.data,
+                    errorLoading: false
+                });
+            })
+            .catch(error => {
+                this.setState({
+                    errorLoading: true
+                });
+            });
+    };
+
+    render({ config }, { vehicles, errorLoading }) {
         return (
             <div>
                 <LayoutGrid>
@@ -22,31 +39,58 @@ export default class VehicleListings extends Component {
                     <LayoutGrid.Inner>
                         <LayoutGrid.Cell cols="12">
                             <div class={style.searchBar}>
-                                <Icon>search</Icon>
+                                <Form action={config.url.api + "vehicle/search"} onSuccess={this.populatePage}>
+                                    <TextField type="text" label={<Icon>search</Icon>} name="term" style="width: calc(100vw - 78px);" />
+                                </Form>
                             </div>
                         </LayoutGrid.Cell>
                     </LayoutGrid.Inner>
                 </LayoutGrid>
                 <LayoutGrid>
                     <LayoutGrid.Inner>
-                        <LayoutGrid.Cell cols="4">
-                            <div class={style.filters}>
-                                ....
-                            </div>
-                            <div class={style.filters}>
-                                ////
-                            </div>
-                            <div class={style.filters}>
-                                !!!!!
-                            </div>
-                        </LayoutGrid.Cell>
-                        <LayoutGrid.Cell cols="8">
-                            <div class={style.results}>
-
-                            </div>
-                        </LayoutGrid.Cell>    
+                        {(() => {
+                            if (!vehicles) {
+                                if (!errorLoading) {
+                                    return (
+                                        <LayoutGrid.Cell cols="12">
+                                            <div class={style.results}>
+                                                Awaiting your query!
+                                            </div>
+                                        </LayoutGrid.Cell>
+                                    );
+                                } else {
+                                    return (
+                                        <LayoutGrid.Cell cols="12">
+                                            <div class={style.results}>
+                                                We encountered an error processing your query. 😕
+                                            </div>
+                                        </LayoutGrid.Cell>
+                                    );
+                                }
+                                return "Awaiting your query!";
+                            } else if (vehicles.length === 0) {
+                                return (
+                                    <LayoutGrid.Cell cols="12">
+                                        <div class={style.results}>
+                                            Your query returned no results.
+                                        </div>
+                                    </LayoutGrid.Cell>
+                                );
+                            } else {
+                                let formattedListings = [];
+                                for (let vehicle in vehicles) {
+                                    formattedListings.push(
+                                        <LayoutGrid.Cell cols="12">
+                                            <a href={config.url.gui + "vehicle/" + vehicle.VIN}>{vehicle.VIN}</a>
+                                        </LayoutGrid.Cell>
+                                    );
+                                }
+                                return formattedListings;
+                            }
+                        })()}
                     </LayoutGrid.Inner>
                 </LayoutGrid>
+
             </div>
         );
     }
