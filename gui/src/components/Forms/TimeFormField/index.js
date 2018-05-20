5c7c65ad42bb282b05d6ed7d5e53b36b401b8f16
Jordan Mele <SiliconSoldier@outlook.com.au>
2018-05-20 17:21:44 +1000

    DateFormField and TimeFormField components

---
+++ gui/src/components/Forms/TimeFormField/index.js
@@ -0,0 +1,22 @@
+import "preact-material-components/TextField/style.css";
+import AbstractField from "../AbstractField";
+import { h } from "preact";
+import TextField from "preact-material-components/TextField";
+import linkState from "linkstate";
+
+/**
+ * Provides a common set of methods and state values to permit the creation of form fields usable within the Form component.
+ */
+export default class TimeFormField extends AbstractField {
+    constructor(props, context) {
+        super(props, context);
+        // Complain about things that will cause quirks.
+        if (props.name === undefined) {
+            console.error("Name is required.");
+        }
+    }
+
+    render(props) {
+        return <TextField {...props} type="time" id={props.name} onChange={linkState(this, "value")} />;
+    }
+}
