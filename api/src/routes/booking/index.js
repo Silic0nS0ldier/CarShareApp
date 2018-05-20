f281198a68400cf4d00ba124e1c5949b2edc16ed
Jordan Mele <SiliconSoldier@outlook.com.au>
2018-05-20 15:23:37 +1000

    Untested get booking info route

--- api/src/routes/booking/index.js
+++ api/src/routes/booking/index.js
@@ -7,9 +7,58 @@ import ssri from "ssri";
 /**
  * Registers all booking related routes.
  */
-export default function register(authGuard, { ImageModel, LogModel, UserModel }) {
+export default function register(authGuard, { BookingModel, ImageModel, UserModel }) {
     const router = express.Router();
 
+    // Bind middleware
+    router.use(authGuard);
+
+    // POST: Create booking
+    router.post("/booking", (req, res) => {
+
+    });
+
+    // GET: Get booking info
+    /** @todo Testing */
+    router.get("/booking/:id", async (req, res) => {
+        if (!req.params.id) {
+            res.sendStatus(400);
+            return;
+        }
+        try {
+            // Fetch booking
+            const booking = await BookingModel.query()
+                .where("id", req.param.id);
+
+            // Make sure exactly 1 record returned
+            if (booking.length > 1) {
+                res.sendStatus(500);
+                return;
+            } else if (booking.length === 0) {
+                res.sendStatus(404);
+                return;
+            }
+
+            // Ensure user allowed (admin is always allowed)
+            if (req.locals.user_id !== 0 ||
+            (booking[0].customer_id !== req.locals.user_id && booking[0].provider_id !== req.locals.user_id)) {
+                res.sendStatus(401);
+                return;
+            }
+
+            // Send data
+            res.status(200).send(booking[0]);
+            return;
+        } catch (error) {
+            res.sendStatus(500);
+            return;
+        }
+    });
+
+    // GET: Get users bookings
+    router.get("/bookings", (req, res) => {
+
+    });
     
     return router;
 };
