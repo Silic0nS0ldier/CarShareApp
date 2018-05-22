34c529fef4d403b507bf660045ed0941a095e9ba
Jordan Mele <SiliconSoldier@outlook.com.au>
2018-05-22 13:59:29 +1000

    Fix other profile and clean up code

--- gui/src/routes/profile/otherprofile.js
+++ gui/src/routes/profile/otherprofile.js
@@ -1,95 +1,42 @@
-import Card from "preact-material-components/Card";
-import Button from "preact-material-components/Button";
-import authorizedFetch from "../../lib/authorizedFetch";
-import { h, Component } from "preact";
 import "preact-material-components/Button/style.css";
 import "preact-material-components/Card/style.css";
-
+import { h, Component } from "preact";
+import authorizedFetch from "../../lib/authorizedFetch";
+import Card from "preact-material-components/Card";
 import style from "./style";
 
 export default class OtherProfile extends Component {
-	/* 
-		// Old login for record/reference
-	state = {
-		time: Date.now(),
-		count: 10
-	};
-
-	// gets called when this route is navigated to
-	componentDidMount() {
-		// start a timer for the clock:
-		this.timer = setInterval(this.updateTime, 1000);
-	}
-
-	// gets called just before navigating away from the route
-	componentWillUnmount() {
-		clearInterval(this.timer);
-	}
-
-	// update the current time
-	updateTime = () => {
-		this.setState({ time: Date.now() });
-	};
-
-	increment = () => {
-		this.setState({ count: this.state.count+1 });
-	};
-
-	// Note: `user` comes from the URL, courtesy of our router
-	render({ user }, { time, count }) {
-		return (
-			<div class={style.profile}>
-				<h1>Profile: {user}</h1>
-				<p>This is the user profile for a user named { user }.</p>
-
-				<div>Current time: {new Date(time).toLocaleString()}</div>
-
-				<p>
-					<Button raised ripple onClick={this.increment}>Click Me</Button>
-					{" "}
-					Clicked {count} times.
-				</p>
-			</div>
-		);
-	} */
-
 	componentDidMount = () => {
-        return authorizedFetch(this.props.config.url.api + "user/" + this.props.userid, {
-            cache: "no-store",
-            headers: {
-                "Accept": "application/json"
-            }
-        }, true).then(
-            (response) => {
-				response.json()
-                    .then((data) => {
-                        if (response.ok) {
-                            this.setState({
-                                profile: data.data
-							});
-							console.log(data.data);
-                        } else {
-                            console.log("Well Shit");
-                        }
-                    })
-                    .catch((error) => {
-                        console.log("Something terrible has happened. Please try again.");
-                        console.log(error);
-                    });
-            },
-            () => {
-                alert("Server is on holidays. Come back later");
-            }
-		).catch(
-			(error) => {
-				console.log(error);
+		return authorizedFetch(this.props.config.url.api + "user/" + this.props.userid, {
+			cache: "no-store",
+			headers: {
+				"Accept": "application/json"
 			}
-		);
-    }
+		}, true)
+			.then(response => {
+				response.json()
+					.then((data) => {
+						if (response.ok) {
+							this.setState({
+								profile: data.data
+							});
+						} else {
+							route("error");
+							console.log("Response NOT ok");
+						}
+					})
+					.catch(error => {
+						route("error");
+						console.log("Failed to fetch profile details.");
+					});
+			}).catch(error => {
+				route("error");
+				console.log("Failed to reach server, cannot fetch profile details.");
+			});
+	}
 
-	render({profile}) {
-		profile = this.state.profile;
-		if(!profile) {
+	render({ }, { profile }) {
+		if (!profile) {
 			return (
 				<div>
 					Loading a user's profile...
@@ -141,13 +88,13 @@ export default class OtherProfile extends Component {
 										let formattedRes = [];
 										profile.reviews = [];
 
-										if(profile.bookingsProvider[0].review != null) {
-											for(let book of profile.bookingsProvider) {
+										if (profile.bookingsProvider.length !== 0) {
+											for (let book of profile.bookingsProvider) {
 												let rev = book.review;
-												profile.reviews.push({sub: rev.subject, comm: rev.comment, rec: rev.recommend});
+												profile.reviews.push({ sub: rev.subject, comm: rev.comment, rec: rev.recommend });
 											}
-										
-											for(let rev of profile.reviews) {
+
+											for (let rev of profile.reviews) {
 												formattedRes.push(
 													<div class="revSep">
 														<p>
@@ -166,7 +113,7 @@ export default class OtherProfile extends Component {
 											}
 										}
 
-										if(profile.reviews.length == 0) {
+										if (profile.reviews.length == 0) {
 											formattedRes.push(
 												<div class="revSep">
 													<p>
